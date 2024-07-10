58fe9b4a1c21f0c25d3624f2cd72518870dc0f26
herdeybayor <shereefdeen4real@gmail.com>
2024-07-10 13:28:15 +0100

    Add @langchain/core dependency and update generatePetName function

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,33 @@
-import { ChatOpenAI } from "@langchain/openai";
+import { StringOutputParser } from "@langchain/core/output_parsers";
+import { PromptTemplate } from "@langchain/core/prompts";
+import { OpenAI } from "@langchain/openai";
 
 import { CONFIGS } from "./configs";
 
-async function generatePetName() {
-    const llm = new ChatOpenAI({
+async function generatePetName(petType: string, petColor: string, numberOfNames: number) {
+    const llm = new OpenAI({
         apiKey: CONFIGS.OPENAI_API_KEY,
         temperature: 0.7,
     });
 
-    const response = await llm.invoke("I have a Cat pet and I want a cool name for it. Suggest me 5 cool names for my pet.");
+    const prompt = new PromptTemplate({
+        inputVariables: ["pet_type", "pet_color", "number_of_names"],
+        template: "I have a {pet_color} {pet_type} pet and I want a cool name for it. Suggest me {number_of_names} cool names for my pet.",
+    });
 
-    console.log(response.content);
+    const outputParser = new StringOutputParser();
+
+    const chain = prompt.pipe(llm).pipe(outputParser);
+
+    const petNames = await chain.invoke({
+        pet_type: petType,
+        pet_color: petColor,
+        number_of_names: numberOfNames,
+    });
+
+    return petNames;
 }
 
-generatePetName();
+generatePetName("dog", "brown", 5).then((petNames) => {
+    console.log(petNames);
+});
